ef0ecfe29030d3bfa2d83ba6b0b2cd919ceff2b1
foram-amipara <foramamipara23@gmail.com>
2026-06-04 20:44:23 +0530

    Time: 0 ms (100%), Space: 30.4 MB (17.9%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;
+        
         if (k == 0) return;
         
         vector<int> temp(n);
