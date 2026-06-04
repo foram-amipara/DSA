e73cc3a213b3cbae34c183656caf1538e7569d0f
foram-amipara <foramamipara23@gmail.com>
2026-06-04 23:49:17 +0530

    Time: 4 ms (9.79%), Space: 50.2 MB (67.13%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+        int count=0;
+        int max1=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count++;
+            }else{
+                count=0;
+            }
+            max1 = max(count,max1);
+        }
+        return max1;
+    }
+};
