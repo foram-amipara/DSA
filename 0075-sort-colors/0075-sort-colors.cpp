b9940237240cda35a199740bdee363b9980c6158
foram-amipara <foramamipara23@gmail.com>
2026-06-07 02:10:58 +0530

    Time: 0 ms (100%), Space: 11.7 MB (10.85%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int freq[3];
-        for (int i=0;i<nums.size();i++){
-            freq[nums[i]]++;
-        }
-        for(int i=0,j=0;i<3;i++){
-            while(freq[i]>0){
-                nums[j]=i;
-                j++;
-                freq[i]--;
+        int n=nums.size();
+        int st=0;
+        int mid=0;
+        int end=n-1;
+
+        while(end>=mid){
+            if(nums[mid]==0){
+                swap(nums[st],nums[mid]);
+                st++;
+                mid++;
             }
+            else if(nums[mid]==2){
+                swap(nums[end],nums[mid]);
+                end--;
+                
+            }else{
+                mid++;
+            }
+            
         }
+
+
+        // int freq[3];
+        // for (int i=0;i<nums.size();i++){
+        //     freq[nums[i]]++;
+        // }
+        // for(int i=0,j=0;i<3;i++){
+        //     while(freq[i]>0){
+        //         nums[j]=i;
+        //         j++;
+        //         freq[i]--;
+        //     }
+        // }
 
     }
 };
