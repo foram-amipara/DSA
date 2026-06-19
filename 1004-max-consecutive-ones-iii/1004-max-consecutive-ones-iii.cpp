1e4f1734a4ae6b5ada3b6ca3520e97a28e32bd55
foram-amipara <foramamipara23@gmail.com>
2026-06-19 19:22:56 +0530

    Time: 0 ms (100%), Space: 69.5 MB (58.92%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0;
+        int count0 = 0;
+        int maxLen = 0;
+        
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] == 0) {
+                count0++;
+            }
+            
+            while (count0 > k) {
+                if (nums[left] == 0) {
+                    count0--;
+                }
+                left++;
+            }
+            
+            maxLen = max(maxLen, right - left + 1);
+        }
+        
+        return maxLen;
+    }
+};
