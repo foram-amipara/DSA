4f0a3b6941fb332be285be037db35de5f4aec51e
foram-amipara <foramamipara23@gmail.com>
2026-06-19 19:11:25 +0530

    Time: 0 ms (100%), Space: 11.4 MB (86.31%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxLength = 0;
+        int left = 0;
+        int freq[256] = {0};
+
+        for (int right = 0; right < s.length(); right++) {
+            freq[s[right]]++;
+
+            while (freq[s[right]] > 1) {
+                freq[s[left]]--;
+                left++;
+            }
+
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};
