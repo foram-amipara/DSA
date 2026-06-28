1637bfd415a1f5df52a3cbf5ac2900d58fda673a
foram-amipara <foramamipara23@gmail.com>
2026-06-28 18:36:03 +0530

    Time: 11 ms (59.31%), Space: 90 MB (64.54%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -6,13 +6,8 @@ public:
         }
         vector<vector<int>> arr(m, vector<int>(n));
 
-        int k = 0;
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                arr[i][j] = original[k];
-                k++;
-            }
+        for(int i=0;i<original.size();i++){
+            arr[i/n][i%n]=original[i];
         }
         return arr;
     }
