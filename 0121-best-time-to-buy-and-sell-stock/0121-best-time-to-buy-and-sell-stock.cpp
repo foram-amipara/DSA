b72e371f8066e4f2bdafc4ae40624fa208e3146d
foram-amipara <foramamipara23@gmail.com>
2026-07-07 16:48:38 +0530

    Time: 2 ms (31.48%), Space: 97.5 MB (25.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int bestBuy[n];
-        bestBuy[0]=INT_MAX;
+        // int n=prices.size();
+        // int bestBuy[n];
+        // bestBuy[0]=INT_MAX;
         
-        for(int i=1;i<n;i++){
-            bestBuy[i]=min(prices[i-1],bestBuy[i-1]);
-        } 
-        int profit =0; 
-        for(int i=0;i<n;i++){
-            int currProfit = prices[i]-bestBuy[i];
-            profit = max(profit,currProfit);
+        // for(int i=1;i<n;i++){
+        //     bestBuy[i]=min(prices[i-1],bestBuy[i-1]);
+        // } 
+        // int profit =0; 
+        // for(int i=0;i<n;i++){
+        //     int currProfit = prices[i]-bestBuy[i];
+        //     profit = max(profit,currProfit);
+        // }
+        // return profit;
+        int right = 0;
+        int left = 0;
+        int ans = 0; 
+        
+        while(right < prices.size()){
+            if(prices[left] > prices[right]){
+                left = right;
+            } else {
+                ans = max(ans, prices[right] - prices[left]);
+            }
+            right++;
         }
-        return profit;
+        
+        return ans;
     }
 };
