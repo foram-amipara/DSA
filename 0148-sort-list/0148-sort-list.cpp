9eba6df9fcdae2877a82d475b272bcc09cd5494a
foram-amipara <foramamipara23@gmail.com>
2026-07-14 22:19:12 +0530

    Time: 8 ms (90.96%), Space: 57 MB (82.84%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+        
+        ListNode *rightHead = splitMid(head);
+        ListNode *right = sortList(rightHead);
+        ListNode *left = sortList(head);
+        
+        return merge(left, right);
+    }
+
+    ListNode* splitMid(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *prev = NULL;
+
+        while (fast != NULL && fast->next != NULL) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        if (prev != NULL) {
+            prev->next = NULL;
+        }
+        
+        return slow;
+    }
+
+    ListNode* merge(ListNode *left, ListNode* right) {
+        ListNode dummy(0);
+        ListNode *tail = &dummy;
+        
+        ListNode *i = left;
+        ListNode *j = right;
+        
+        while (i != NULL && j != NULL) {
+            if (i->val <= j->val) {
+                tail->next = i;
+                i = i->next;
+            } else {
+                tail->next = j;
+                j = j->next;
+            }
+            tail = tail->next;
+        }
+        
+        if (i != NULL) {
+            tail->next = i;
+        } else {
+            tail->next = j;
+        }
+        
+        return dummy.next;
+    }
+};
