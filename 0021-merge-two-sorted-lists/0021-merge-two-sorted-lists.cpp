b11c6ad5800c11ea38afbdf06c35a98e4103de72
foram-amipara <foramamipara23@gmail.com>
2026-07-19 16:38:41 +0530

    Time: 0 ms (100%), Space: 19.5 MB (62.99%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,59 +11,82 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == NULL) return list2;
-        if (list2 == NULL) return list1;
+        // if (list1 == NULL) return list2;
+        // if (list2 == NULL) return list1;
 
-        ListNode *temp1 = list1;
-        ListNode *temp2 = list2;
-        ListNode *prev = NULL;
-        ListNode *head = NULL;
+        // ListNode *temp1 = list1;
+        // ListNode *temp2 = list2;
+        // ListNode *prev = NULL;
+        // ListNode *head = NULL;
         
-        while (temp1 != NULL && temp2 != NULL) {
-            ListNode *next1 = temp1->next;
-            ListNode *next2 = temp2->next;
+        // while (temp1 != NULL && temp2 != NULL) {
+        //     ListNode *next1 = temp1->next;
+        //     ListNode *next2 = temp2->next;
             
-            if (temp1->val == temp2->val) {
-                if (prev != NULL) {
-                    prev->next = temp1;
-                    temp1->next = temp2;
-                    prev = temp2;
-                } else {
-                    head = temp1;
-                    temp1->next = temp2;
-                    prev = temp2;
-                }
-                temp1 = next1;
-                temp2 = next2;
+        //     if (temp1->val == temp2->val) {
+        //         if (prev != NULL) {
+        //             prev->next = temp1;
+        //             temp1->next = temp2;
+        //             prev = temp2;
+        //         } else {
+        //             head = temp1;
+        //             temp1->next = temp2;
+        //             prev = temp2;
+        //         }
+        //         temp1 = next1;
+        //         temp2 = next2;
+        //     }
+        //     else if (temp1->val < temp2->val) {
+        //         if (prev != NULL) {
+        //             prev->next = temp1;
+        //             prev = temp1;
+        //         } else {
+        //             head = temp1;
+        //             prev = temp1;
+        //         }
+        //         temp1 = next1;
+        //     }
+        //     else if (temp1->val > temp2->val) {
+        //         if (prev != NULL) {
+        //             prev->next = temp2;
+        //             prev = temp2;
+        //         } else {
+        //             head = temp2;
+        //             prev = temp2;
+        //         }
+        //         temp2 = next2;
+        //     }
+        // }
+
+        // if (temp1 != NULL) {
+        //     prev->next = temp1;
+        // } else {
+        //     prev->next = temp2;
+        // }
+
+        // return head;
+        ListNode* dummy=new ListNode(0);
+        ListNode* prev =dummy; 
+        ListNode* temp1=list1;
+        ListNode* temp2=list2;
+        while(temp1!=NULL && temp2!=NULL){
+            if(temp1->val<=temp2->val){
+                prev->next=temp1;
+                temp1=temp1->next;
+            }else{
+                prev->next=temp2;
+                temp2=temp2->next;
             }
-            else if (temp1->val < temp2->val) {
-                if (prev != NULL) {
-                    prev->next = temp1;
-                    prev = temp1;
-                } else {
-                    head = temp1;
-                    prev = temp1;
-                }
-                temp1 = next1;
-            }
-            else if (temp1->val > temp2->val) {
-                if (prev != NULL) {
-                    prev->next = temp2;
-                    prev = temp2;
-                } else {
-                    head = temp2;
-                    prev = temp2;
-                }
-                temp2 = next2;
-            }
+            prev=prev->next;
         }
 
-        if (temp1 != NULL) {
-            prev->next = temp1;
-        } else {
-            prev->next = temp2;
+        if(temp1!=NULL){
+            prev->next=temp1;
+        }else{
+            prev->next=temp2;
         }
+        return dummy->next;
 
-        return head;
+
     }
 };
