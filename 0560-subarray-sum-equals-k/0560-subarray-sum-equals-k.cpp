e6567321c3a8d2dc0a41b82a1fdfff8dbf773412
foram-amipara <foramamipara23@gmail.com>
2026-07-21 17:24:38 +0530

    Time: 35 ms (86.87%), Space: 45.5 MB (40.76%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,18 +4,18 @@
 class Solution {
 public:
     int subarraySum(std::vector<int>& nums, int k) {
-        std::unordered_map<int, int> prefix_counts;
-        prefix_counts[0] = 1;
+        std::unordered_map<int, int> prefCount;
+        prefCount[0] = 1;
         
-        int current_sum = 0;
+        int currSum = 0;
         int count = 0;
         
         for (int num : nums) {
-            current_sum += num;
-            if (prefix_counts.find(current_sum - k) != prefix_counts.end()) {
-                count += prefix_counts[current_sum - k];
+            currSum += num;
+            if (prefCount.find(currSum - k) != prefCount.end()) {
+                count += prefCount[currSum - k];
             }
-            prefix_counts[current_sum]++;
+            prefCount[currSum]++;
         }
         
         return count;
