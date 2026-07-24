2c10249c2d3d74b50ce8052fa47403d2037907e8
foram-amipara <foramamipara23@gmail.com>
2026-07-24 21:14:44 +0530

    Time: 1 ms (53.8%), Space: 12.9 MB (15.74%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums2.size();
         stack<int> s;
-        unordered_map<int, int> next_greater;
-        for(int i=0;i<nums2.size();i++){
-            while(!s.empty() && s.top()<nums2[i]){
-                next_greater[s.top()] = nums2[i];
+        unordered_map<int, int> ngr;
+
+        ngr[nums2[n - 1]] = -1;
+        s.push(nums2[n - 1]);
+
+        for (int i = n - 2; i >= 0; i--) {
+            int curr = nums2[i];
+
+            while (!s.empty() && curr >= s.top()) {
                 s.pop();
             }
-            s.push(nums2[i]);
-        }
-        vector<int> ans;
-        for(int j=0;j<nums1.size();j++){
-            if(next_greater.count(nums1[j])){
-                ans.push_back(next_greater[nums1[j]]);
-            }else{
-                ans.push_back(-1);
-            }
-        }
-        return ans;
 
+            if (s.empty()) {
+                ngr[curr] = -1;
+            } else {
+                ngr[curr] = s.top();
+            }
+
+            s.push(curr);
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < nums1.size(); i++) {
+            ans.push_back(ngr[nums1[i]]);
+        }
+
+        return ans;
     }
 };
