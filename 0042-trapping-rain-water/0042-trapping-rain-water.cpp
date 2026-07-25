325e989bb6c35ac13a83753af177023a136f1056
foram-amipara <foramamipara23@gmail.com>
2026-07-25 19:26:55 +0530

    Time: 0 ms (100%), Space: 27.6 MB (6.78%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,21 +2,39 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n= height.size();
-        int rightMax[n];
-        int leftMax[n];
-        leftMax[0]=height[0];
-        rightMax[n-1]=height[n-1];
-        //left max
+        vector<int> rm(n,0);
+        vector<int> lm(n,0);
+        stack<int> s; 
+
+        lm[0]=height[0];
+        rm[n-1]=height[n-1];
+
+        s.push(height[0]);
+        //lm
         for(int i=1;i<n;i++){
-            leftMax[i]=max(leftMax[i-1],height[i-1]);
+            int curr=height[i];
+            if (height[i] > s.top()) {
+            s.pop();
+            s.push(height[i]);
+            }
+            lm[i] = s.top();
         }
-        //right max
-        for(int i=n-2;i>=0;i--){
-            rightMax[i]=max(rightMax[i+1],height[i+1]);
+        while(!s.empty()){
+            s.pop();
         }
+        s.push(height[n-1]);
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (height[i] > s.top()) {
+                s.pop();
+                s.push(height[i]);
+            }
+            rm[i] = s.top();
+        }
+       
         int water=0;
         for(int i=0;i<n;i++){
-            int currWater = min(leftMax[i],rightMax[i])-height[i];
+            int currWater = min(lm[i],rm[i])-height[i];
             if(currWater>0){
                 water+=currWater;
             }
@@ -24,3 +42,28 @@ public:
         return water;
     }
 };
+
+
+
+
+// int n= height.size();
+//         int rightMax[n];
+//         int leftMax[n];
+//         leftMax[0]=height[0];
+//         rightMax[n-1]=height[n-1];
+//         //left max
+//         for(int i=1;i<n;i++){
+//             leftMax[i]=max(leftMax[i-1],height[i-1]);
+//         }
+//         //right max
+//         for(int i=n-2;i>=0;i--){
+//             rightMax[i]=max(rightMax[i+1],height[i+1]);
+//         }
+//         int water=0;
+//         for(int i=0;i<n;i++){
+//             int currWater = min(leftMax[i],rightMax[i])-height[i];
+//             if(currWater>0){
+//                 water+=currWater;
+//             }
+//         }
+//         return water;
