6aaa19efb8218d80cf1e3f2200d5ece6da724d65
foram-amipara <foramamipara23@gmail.com>
2026-07-25 20:03:17 +0530

    Time: 758 ms (5.42%), Space: 783.7 MB (5.1%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         stack<char> st;
-        
-        for(int i=0;i<s.length();i++){
-            char curr=s[i];
-            if(!st.empty() && curr == st.top()){
-               st.pop();
-            }else{
-                st.push(curr);  
+
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s[i];
+            
+            if (!st.empty() && curr == st.top()) { 
+                st.pop();
+            } else {
+                st.push(curr);
             }
         }
-        string ans="";
-        while(!st.empty()){
-            ans=st.top()+ans;
+
+        string ans = "";
+        while (!st.empty()) {
+            ans = st.top() + ans;
             st.pop();
         }
+
         return ans;
     }
 };
