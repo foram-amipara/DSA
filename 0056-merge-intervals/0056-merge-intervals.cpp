b4ac10c8f16c339898a7b815324e4ce048fc4b0b
foram-amipara <foramamipara23@gmail.com>
2026-08-07 23:25:48 +0530

    Time: 0 ms (100%), Space: 25.5 MB (5.07%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        vector<pair<int,int>> pairs(intervals.size());
+        for(int i=0;i<intervals.size();i++){
+            pairs[i]=make_pair(intervals[i][0],intervals[i][1]);
+        }
+        getAns(pairs,ans);
+        return ans;
+
+    }
+    void getAns(vector<pair<int,int>> &pairs,vector<vector<int>> &ans){
+        if(pairs.empty()) return;
+        int n=pairs.size();
+        sort(pairs.begin(),pairs.end(),compare);
+        int currEnd=pairs[0].second;
+        int currSt=pairs[0].first;
+
+        for(int i=1;i<n;i++){
+            if(pairs[i].first<=currEnd){
+                currEnd=max(currEnd,pairs[i].second);
+            }else{
+                ans.push_back({currSt,currEnd});
+                currSt=pairs[i].first;
+                currEnd=pairs[i].second;
+            }
+        }
+        ans.push_back({currSt,currEnd});
+
+    }
+    static bool compare(const pair<int,int> &p1,pair<int,int> &p2){
+        return p1.first<p2.first;
+    }
+};
