ec5bb60aec0b88413007701dda4b3aa5ecf3d244
foram-amipara <foramamipara23@gmail.com>
2026-08-11 11:28:36 +0530

    Time: 0 ms (100%), Space: 13.3 MB (29.64%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int maxNum=*max_element(arr.begin(),arr.end());
-        int x=maxNum + k;
-        int j=0;
-        int count=0;
-        for(int i=1;i<=x;i++){
-            if(j<arr.size()&&arr[j]==i){
-                j++;
-                continue;
+        int st=0;
+        int end=arr.size()-1;
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if((arr[mid]-mid-1)>=k){
+                end=mid-1;
             }else{
-                count++;
-            }
-            if(count==k){
-                return i;
+                st=mid+1;
             }
         }
-        return 0;
+        return st+k;
     }
 };
+
+
+// int maxNum=*max_element(arr.begin(),arr.end());
+//         int x=maxNum + k;
+//         int j=0;
+//         int count=0;
+//         for(int i=1;i<=x;i++){
+//             if(j<arr.size()&&arr[j]==i){
+//                 j++;
+//                 continue;
+//             }else{
+//                 count++;
+//             }
+//             if(count==k){
+//                 return i;
+//             }
+//         }
+//         return 0;
