9550b2cb5600c56e5cd89afc3deb84a5281ead6c
foram-amipara <foramamipara23@gmail.com>
2026-08-13 18:50:04 +0530

    Time: 10 ms (37.93%), Space: 15.9 MB (5.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        int n = nums.size(); 
-        
-       
-        for(int i = 0; i < n; i++) {
-           
-            for(int j = i + 1; j < n; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        unordered_map<int,int> m;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            int comp = target - nums[i];
+            if(m.count(comp)){
+                ans.push_back(i);
+                ans.push_back(m[comp]);
             }
+            m[nums[i]]=i;
         }
-       
-        return {};
+        return ans;
     }
-}; 
+};
+
+// for(int i = 0; i < nums.size(); i++) {
+//             for(int j = i + 1; j < nums.size(); j++) {
+//                 if(nums[i] + nums[j] == target) {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {};
