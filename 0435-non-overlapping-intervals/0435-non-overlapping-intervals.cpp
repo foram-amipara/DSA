e6ea7557800fe5f2d7ded8626799cc258233ddf0
foram-amipara <foramamipara23@gmail.com>
2026-08-14 20:50:34 +0530

    Time: 75 ms (15.51%), Space: 113.2 MB (10.13%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,56 +1,28 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return 0;
+        vector<pair<int,int>> v;
 
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];
+        for(auto x : intervals) {
+            v.push_back({x[0], x[1]});
+        }
+
+        sort(v.begin(), v.end(), [](pair<int,int>& a, pair<int,int>& b) {
+            return a.second < b.second;
         });
 
         int count = 0;
-        int currEnd = intervals[0][1];
+        int prevEnd = v[0].second;
 
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] >= currEnd) {
-                currEnd = intervals[i][1];
-            } else {
+        for(int i = 1; i < v.size(); i++) {
+            if(v[i].first < prevEnd) {
                 count++;
+            }
+            else {
+                prevEnd = v[i].second;
             }
         }
 
         return count;
     }
-    
 };
-
-
-static bool compare(const pair<int, int> &p1, const pair<int, int> &p2) {
-        return p1.second < p2.second;
-    }
-
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return 0;
-
-        int n = intervals.size();
-        vector<pair<int, int>> pairs(n);
-        
-        for (int i = 0; i < n; i++) {
-            pairs[i] = make_pair(intervals[i][0], intervals[i][1]);
-        }
-
-        sort(pairs.begin(), pairs.end(), compare);
-
-        int count = 0;
-        int currEnd = pairs[0].second;
-
-        for (int i = 1; i < n; i++) {
-            if (pairs[i].first >= currEnd) {
-                
-                currEnd = pairs[i].second; 
-            } else {
-                count++;
-            }
-        }
-
-        return count;
-    }
