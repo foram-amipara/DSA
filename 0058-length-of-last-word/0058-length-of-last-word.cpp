a872c9716c0b6e400087143b2d6028c22c360ce4
foram-amipara <foramamipara23@gmail.com>
2026-08-15 21:17:56 +0530

    Time: 0 ms (100%), Space: 8.8 MB (69.05%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1;
+        while (s[i] == ' ') {
+            i--;
+        }
+        int count = 0;
+        for (int j = i; j >= 0; j--) {
+            if (s[j] == ' ') {
+                break;
+            }
+            count++;
+        }
+
+        return count;
+    }
+};
