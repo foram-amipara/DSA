a76d847b9375f87d8c284e306bfd89bed9eadd2b
foram-amipara <foramamipara23@gmail.com>
2026-08-17 18:29:59 +0530

    Time: 0 ms (100%), Space: 20.6 MB (79.04%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -4,7 +4,7 @@ public:
         int jumps = 0;
         int farthest = 0;
         int currentEnd = 0;
-         for (int i = 0; i < nums.size() - 1; i++) {
+        for (int i = 0; i < nums.size() - 1; i++) {
             farthest = max(farthest, i + nums[i]);
 
             if (i == currentEnd) {
@@ -12,7 +12,6 @@ public:
                 currentEnd = farthest;
             }
         }
-
         return jumps;
     }
 };
