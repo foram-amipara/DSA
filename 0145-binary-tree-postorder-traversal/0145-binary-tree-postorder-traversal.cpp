4655b1b7608a07e2e620eb0370532e785c2d5e83
foram-amipara <foramamipara23@gmail.com>
2026-08-19 16:53:16 +0530

    Time: 0 ms (100%), Space: 11 MB (46.05%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -12,17 +12,72 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
+        if(!root){
+            return {};
+        }
         vector<int> ans;
-        postOrder(root,ans);
+        stack<TreeNode*> s1,s2;
+
+        s1.push(root);
+
+        while(!s1.empty()){
+            TreeNode *curr=s1.top();
+            s1.pop();
+            s2.push(curr);
+
+            if(curr->left!=NULL){
+                s1.push(curr->left);
+            }
+            if(curr->right !=NULL){
+                s1.push(curr->right);
+            }
+        }
+        while(!s2.empty()){
+            ans.push_back(s2.top()->val);
+            s2.pop();
+        }
         return ans;
     }
-    void postOrder(TreeNode *root,vector<int> &ans){
-        if(root==NULL){
-            return;
-        }
-        postOrder(root->left,ans);
-        postOrder(root->right,ans);
-        ans.push_back(root->val);
-        
-    }
 };
+
+
+
+
+// vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         postOrder(root,ans);
+//         return ans;
+//     }
+//     void postOrder(TreeNode *root,vector<int> &ans){
+//         if(root==NULL){
+//             return;
+//         }
+//         postOrder(root->left,ans);
+//         postOrder(root->right,ans);
+//         ans.push_back(root->val);
+        
+//     }
+
+
+
+// vector<int> ans;
+//         stack<TreeNode*> s;
+//         TreeNode* curr = root;
+//         TreeNode *prev=NULL;
+
+//         while (curr != NULL || !s.empty()) {
+//             while (curr != NULL) {
+//                 s.push(curr);
+//                 curr=curr->left;
+//             }
+//             curr=s.top();
+//             if(curr->right !=NULL && curr->right != prev){
+//                 curr=curr->right;
+//             }else{
+//                 ans.push_back(curr->val);
+//                 s.pop();
+//                 prev=curr;
+//                 curr=NULL;
+//             }
+//         }
+//         return ans;
