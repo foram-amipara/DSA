bb5a82ab5ddac7731e5f30b282800d7d96c22422
foram-amipara <foramamipara23@gmail.com>
2026-08-20 16:21:42 +0530

    Time: 0 ms (100%), Space: 23.1 MB (55.77%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,35 +12,65 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        return balancedNode(root);
-    }
-    bool balancedNode(TreeNode *root){
-        if(root==NULL){
+        if(checkHeight(root)!=-1){
             return true;
         }
-        
-        int leftDepth=calculateDepth(root->left);
-        int rightDepth=calculateDepth(root->right);
-        if(leftDepth-rightDepth==1 || leftDepth-rightDepth== -1 || leftDepth-rightDepth==0 ){
-            bool left=balancedNode(root->left);
-            bool right=balancedNode(root->right);
-            if(left && right){
-                return true;
-            }else{return false;}
-            
-        }
         return false;
-        
     }
-    int calculateDepth(TreeNode* root){
+    int checkHeight(TreeNode* root) {
         if(root==NULL){
             return 0;
         }
-        int leftHt=calculateDepth(root->left);
-        int rightHt=calculateDepth(root->right);
-        int height=max(leftHt,rightHt)+1;
+        int left=checkHeight(root->left);
+        if(left==-1){
+            return -1;
+        }
+        int right=checkHeight(root->right);
+        if(right==-1){
+            return -1;
+        }
 
-        return height;
+        if(abs(left-right)>1){
+            return -1;
+        }
+
+        return max(left,right)+1;
 
     }
 };
+
+
+
+
+// bool isBalanced(TreeNode* root) {
+//         return balancedNode(root);
+//     }
+//     bool balancedNode(TreeNode *root){
+//         if(root==NULL){
+//             return true;
+//         }
+        
+//         int leftDepth=calculateDepth(root->left);
+//         int rightDepth=calculateDepth(root->right);
+//         if(leftDepth-rightDepth==1 || leftDepth-rightDepth== -1 || leftDepth-rightDepth==0 ){
+//             bool left=balancedNode(root->left);
+//             bool right=balancedNode(root->right);
+//             if(left && right){
+//                 return true;
+//             }else{return false;}
+            
+//         }
+//         return false;
+        
+//     }
+//     int calculateDepth(TreeNode* root){
+//         if(root==NULL){
+//             return 0;
+//         }
+//         int leftHt=calculateDepth(root->left);
+//         int rightHt=calculateDepth(root->right);
+//         int height=max(leftHt,rightHt)+1;
+
+//         return height;
+
+//     }
