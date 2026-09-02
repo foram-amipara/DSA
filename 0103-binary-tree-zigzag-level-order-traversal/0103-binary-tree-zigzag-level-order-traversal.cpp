59d1ec9967270efbc5375b9105869087ec96fd0c
foram-amipara <foramamipara23@gmail.com>
2026-09-02 23:50:51 +0530

    Time: 0 ms (100%), Space: 15.1 MB (78.07%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,31 +12,86 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if (root == nullptr) return result;
+        vector<vector <int>> ans;
+        getAns(root,ans);
+        return ans;
 
+    }
+    void getAns(TreeNode *root, vector<vector<int>> &ans){
+        if(root==NULL) return;
+        bool leftToRight=true;
         queue<TreeNode*> q;
         q.push(root);
-        bool ltr = true;
+        q.push(NULL);
+        vector<int> currVec;
 
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> level(size);
+        while(!q.empty()){
+            TreeNode* curr=q.front();
+            q.pop();
+            
+            
+            if(curr==NULL){
+                if(!leftToRight){
+                    reverse(currVec.begin(), currVec.end());
+                }
+                ans.push_back(currVec);
+                currVec.clear();
 
-            for (int i = 0; i < size; ++i) {
-                TreeNode* curr = q.front();
-                q.pop();
-                int index = ltr ? i : (size - 1 - i);
-                level[index] = curr->val;
-
-                if (curr->left) q.push(curr->left);
-                if (curr->right) q.push(curr->right);
+                if(q.empty()){
+                    break;
+                }
+                q.push(NULL);
+                leftToRight=!leftToRight;
+            }else{
+                
+                currVec.push_back(curr->val);
+                if(curr->left!=NULL){
+                q.push(curr->left);
+                }
+                if(curr->right!=NULL){
+                    q.push(curr->right);
+                }
             }
-
-            result.push_back(level);
-            ltr = !ltr;
+           
         }
 
-        return result;
     }
+
 };
+
+
+
+
+
+
+
+
+
+
+// vector<vector<int>> result;
+//         if (root == nullptr) return result;
+
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         bool ltr = true;
+
+//         while (!q.empty()) {
+//             int size = q.size();
+//             vector<int> level(size);
+
+//             for (int i = 0; i < size; ++i) {
+//                 TreeNode* curr = q.front();
+//                 q.pop();
+//                 int index = ltr ? i : (size - 1 - i);
+//                 level[index] = curr->val;
+
+//                 if (curr->left) q.push(curr->left);
+//                 if (curr->right) q.push(curr->right);
+//             }
+
+//             result.push_back(level);
+//             ltr = !ltr;
+//         }
+
+//         return result;
+
