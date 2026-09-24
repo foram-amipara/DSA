abc34c049ac28fe5694329b5f0ca7204a3642b4c
foram-amipara <foramamipara23@gmail.com>
2026-09-24 22:41:42 +0530

    Time: 2 ms (59.03%), Space: 18.1 MB (34.07%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> count;
+        for (int num : nums) {
+            count[num]++;
+        }
+        priority_queue<pair<int,int>, vector<pair<int,int>>,greater<pair<int,int>>> minHeap;
+
+        for (auto& n : count){
+            minHeap.push({n.second,n.first});
+            if(minHeap.size()>k){
+                minHeap.pop();
+            }
+        }
+        vector<int> ans;
+        while(!minHeap.empty()){
+            ans.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+        return ans;
+
+    }
+};
